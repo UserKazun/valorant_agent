895f45d86aeb137c26b907e3c11557950aa739d6
UserKazun <kazuusjlo8@gmail.com>
2022-08-04 21:53:28 +0900

    connect db, create new api

--- valorant_agent.go
+++ valorant_agent.go
@@ -1,12 +1,19 @@
 package valorant
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
+	"os"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 type Renderer struct {
@@ -21,24 +28,73 @@ type ServiceInfo struct {
 	Title string
 }
 
+type Agent struct {
+	ID         int    `db:"id"`
+	Name       string `db:"name"`
+	Background string `db:"background"`
+	CreatedAt  string `db:"created_at"`
+	UpdatedAt  string `db:"updated_at"`
+}
+
 var (
-	tr = &Renderer{templates: template.Must(template.ParseGlob("../views/*.html"))}
-	db *sqlx.DB
+	tr  = &Renderer{templates: template.Must(template.ParseGlob("../views/*.html"))}
+	db  *sqlx.DB
+	err error
 )
 
 var serviceInfo = ServiceInfo{
 	"Valorant Agent",
 }
 
+func getEnv(key string, defaultValue string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return defaultValue
+}
+
+// 管理用DBに接続する
+func connectDB() (*sqlx.DB, error) {
+	config := mysql.NewConfig()
+	config.Net = "tcp"
+	config.Addr = getEnv("DB_HOST", "127.0.0.1") + ":" + getEnv("DB_PORT", "3306")
+	config.User = getEnv("DB_USER", "agent")
+	config.Passwd = getEnv("DB_PASSWORD", "agent")
+	config.DBName = getEnv("DB_NAME", "valorant")
+	config.ParseTime = true
+	dsn := config.FormatDSN()
+
+	return sqlx.Open("mysql", dsn)
+}
+
 func Run() {
 	e := echo.New()
+	e.Debug = true
+	e.Logger.SetLevel(log.DEBUG)
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.Renderer = tr
 
 	e.GET("/", example)
 	e.GET("/home", home)
 
+	e.GET("/api/agents", getAgents)
+
+	db, err = connectDB()
+	if err != nil {
+		e.Logger.Fatal("faild to connect db: %v", err)
+		return
+	}
+	defer db.Close()
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type SuccessResult struct {
+	Status bool `json:"status"`
+	Data   any  `json:"data"`
 }
 
 // TODO: 後で消す
@@ -56,4 +112,21 @@ func home(c echo.Context) error {
 	}
 
 	return c.Render(http.StatusOK, "home", data)
+}
+
+func getAgents(c echo.Context) error {
+	ctx := context.Background()
+	agentRow := []Agent{}
+	if err := db.SelectContext(
+		ctx,
+		&agentRow,
+		"SELECT * FROM agents",
+	); err != nil && err != sql.ErrNoRows {
+		return fmt.Errorf("error agents: %v", err)
+	}
+
+	return c.JSON(http.StatusOK, SuccessResult{
+		Status: true,
+		Data:   &agentRow,
+	})
 }
