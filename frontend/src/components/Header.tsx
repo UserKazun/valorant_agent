865a61e0af50b6b3b90b191b50c221001c1b381c
UserKazun <kazuusjlo8@gmail.com>
2022-08-14 14:50:11 +0900

    add Header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+
+const Header = ({ siteTitle }: { siteTitle: string }) => (
+    <header
+    style={{
+      background: `#BD3944`,
+      marginBottom: `1.45rem`,
+      color: `#FFFFFF`,
+    }}
+    >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 1440,
+        maxHeight: 64,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1
+      style={{
+        margin: 0,
+        textAlign: 'left'
+        }}>
+        {siteTitle}
+        {/* <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link> */}
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+    siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+    siteTitle: `VALORANT AGENT`,
+}
+
+export default Header
---
+++ frontend/src/components/layout.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header"
+import "./layout.css"
+
+const Layout = () => {
+    return (
+        <Header siteTitle={`VALORANT AGENT`} />
+    )
+}
+
+export default Layout
