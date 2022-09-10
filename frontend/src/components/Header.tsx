a37ead8b81057067602789697a43cc8df632da33
UserKazun <kazuusjlo8@gmail.com>
2022-09-10 20:33:09 +0900

    delete comment out

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,31 +13,6 @@ export default function Header(): JSX.Element {
         </ButtonWrapper>
       </Wrapper>
     );
-  // <header
-  //   style={{
-  //     background: `#BD3944`,
-  //     marginBottom: `1.45rem`,
-  //     color: `#FFFFFF`,
-  //   }}
-  //   >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 1440,
-  //       maxHeight: 64,
-  //       padding: `1.45rem 1.0875rem`,
-  //     }}
-  //   >
-  //     <h1
-  //     style={{
-  //       margin: 0,
-  //       textAlign: 'left'
-  //       }}>
-  //       {props.siteTitle}
-  //     </h1>
-  //     <SignUpButton></SignUpButton>
-  //   </div>
-  // </header>
 };
 
 const Wrapper = styled.header`
