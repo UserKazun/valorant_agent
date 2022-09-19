50a37e0d2a0f52df121a22ba11fb74fa4d4b6b33
UserKazun <kazuusjlo8@gmail.com>
2022-09-19 21:29:48 +0900

    create new register, login endpoint

--- cmd/model/model.go
+++ cmd/model/model.go
@@ -12,3 +12,12 @@ type Agent struct {
 	CreatedAt  string `db:"created_at" json:"create_at"`
 	UpdatedAt  string `db:"updated_at" json:"updated_at"`
 }
+
+type User struct {
+	ID        int    `db:"id" json:"id"`
+	UserName  string `db:"user_name" json:"user_name"`
+	Email     string `db:"email" json:"email"`
+	Password  string `db:"password" json:"password"`
+	CreatedAt string `db:"created_at" json:"created_at"`
+	UpdatedAt string `db:"updated_at" json:"updated_at"`
+}
